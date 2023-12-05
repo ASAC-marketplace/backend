3e42a127cf7b97b7f451f8929c6a54c77c60b310
pobregato73 <pobregato73@gmail.com>
2023-12-05 21:40:11 +0900

    Feat: 모든 조건 검색 기능 추가

---
+++ src/main/java/market/demo/domain/search/ItemSearchCondition.java
@@ -0,0 +1,21 @@
+package market.demo.domain.search;
+
+import lombok.Data;
+import market.demo.domain.status.ItemStatus;
+import market.demo.domain.type.PromotionType;
+
+import java.time.LocalDate;
+
+@Data
+public class ItemSearchCondition {
+    private String name;
+    private Long categoryId;
+    private ItemStatus status;
+    private PromotionType promotionType;
+    private Integer minStockQuantity;
+    private LocalDate minRegisteredDate;
+    private Integer minDiscountRate;
+    private Integer maxDiscountRate;
+    private Integer minPrice;
+    private Integer maxPrice;
+}
