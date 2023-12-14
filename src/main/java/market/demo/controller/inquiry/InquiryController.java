8b48bc39f756cfc12b9c77d206f0b14f0dcd42ab
gato <111480377+kurikame73@users.noreply.github.com>
2023-12-14 23:37:43 +0900

    Feat: 리스트 조회 디테일 조회

--- src/main/java/market/demo/controller/inquiry/InquiryController.java
+++ src/main/java/market/demo/controller/inquiry/InquiryController.java
@@ -2,10 +2,15 @@ package market.demo.controller.inquiry;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import market.demo.dto.inquiry.InquiryDetailResponse;
+import market.demo.dto.inquiry.InquiryListResponse;
 import market.demo.dto.inquiry.InquiryRequest;
 import market.demo.service.inquiry.InquiryService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +23,21 @@ public class InquiryController {
     public ResponseEntity<?> createInquiry(@Valid @RequestBody InquiryRequest inquiryRequest) {
         inquiryService.createInquiry(inquiryRequest);
         return ResponseEntity.ok("문의 등록 성공");
+    }
+
+    @GetMapping("/list/{memberId}")
+    public ResponseEntity<Page<InquiryListResponse>> getInquiriesByMemberId(
+            @PathVariable Long memberId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Page<InquiryListResponse> inquiries = inquiryService.getInquiryList(memberId, page, size);
+        return ResponseEntity.ok(inquiries);
+    }
+
+    @GetMapping("/{inquiryId}/")
+    public ResponseEntity<InquiryDetailResponse> getInquiryDetails(@PathVariable Long inquiryId) {
+        InquiryDetailResponse inquiryDetail = inquiryService.getInquiryDetails(inquiryId);
+        return ResponseEntity.ok(inquiryDetail);
     }
 
 //    @GetMapping()
