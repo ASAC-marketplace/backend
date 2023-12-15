ef494bec71b76c12ae84205ab4dec8cd5b308aa6
Sarah <aases297500@naver.com>
2023-12-15 15:04:29 +0900

    test

--- src/main/java/market/demo/dto/inquiry/InquiryListResponse.java
+++ src/main/java/market/demo/dto/inquiry/InquiryListResponse.java
@@ -25,15 +25,4 @@ public class InquiryListResponse {
         response.status = inquiry.getStatus();
         return response;
     }
-<<<<<<< HEAD
-=======
-
-//    // 매개변수를 받는 생성자
-//    public InquiryListResponse(Inquiry inquiry) {
-//        this.id = inquiry.getId();
-//        this.title = inquiry.getTitle();
-//        this.registerDate = inquiry.getRegisterDate();
-//        this.status = inquiry.getStatus();
-//    }
->>>>>>> origin/main
 }
